a3532f6496fd4576c34759e1668ab8cd2b362c3b
AdhiGozalt <Muhammadadhigozalt02@gmail.com>
2024-06-06 21:37:32 +0700

    Creating List for Available Orphanage

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { getAllPantiAsuhan, getPantiAsuhanById } = require('../controllers/listController');
 
 // Endpoint untuk mendapatkan semua data panti asuhan
-router.get('/panti-asuhan', controller.getAllPantiAsuhan);
+router.get('/panti-asuhan', getAllPantiAsuhan);
 
 // Endpoint untuk mendapatkan detail panti asuhan berdasarkan ID
-router.get('/panti-asuhan/:id', controller.getPantiAsuhanById);
+router.get('/panti-asuhan/:id', getPantiAsuhanById);
 
 module.exports = router; // Export router agar dapat digunakan oleh aplikasi Express
