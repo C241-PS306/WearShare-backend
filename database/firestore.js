dd611b12e68719481ff18a7cf535e0418a950343
wahyu bagus <bagus8545@gmail.com>
2024-06-07 11:15:19 +0700

    Creating predict and history

--- database/firestore.js
+++ database/firestore.js
@@ -1,12 +1,16 @@
 // firestore.js
 const admin = require('firebase-admin');
+const { Storage } = require('@google-cloud/storage');
 const serviceAccount=require('../service-account.json')
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'wearshare-bucket' // Ganti dengan nama bucket Anda
 });
 
 const db = admin.firestore();
+const storage = new Storage();
 
-module.exports = db;
+module.exports = { admin, db, storage };
+
 
