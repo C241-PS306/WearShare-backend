5548870a5344421470cc45720826dff657f02e48
wahyu bagus <bagus8545@gmail.com>
2024-06-10 23:16:29 +0700

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,29 @@
-// middlewares/authMiddleware.js
-exports.authenticate = (req, res, next) => {
-  if (!req.session.user) {
-    return res.status(401).json({ message: 'Unauthorized' });
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const auth = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+  if(!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid');
   }
-  next();
-};
+
+  // split the token
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach the user to the job routes
+
+    const user = User.findById(payload.id).select('-password');
+    req.user = user;
+
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+}
+
+module.exports = auth;
